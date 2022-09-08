fed06ab166ea5a557c70a670311d77d9b40554b4
mahdidlr <89592833+MahdiidhaM@users.noreply.github.com>
2022-09-08 23:07:21 +0430

    add jwt

--- Shop/settings.py
+++ Shop/settings.py
@@ -98,10 +98,16 @@ WSGI_APPLICATION = 'Shop.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': '',
     }
+}
 }
 
 
