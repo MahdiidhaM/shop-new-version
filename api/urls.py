e55607510c81a9790933fd98d255550294998f5a
mahdidlr <89592833+MahdiidhaM@users.noreply.github.com>
2022-09-10 20:25:53 +0430

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,11 @@ from .views import *
 
 
 urlpatterns = [
-    path('',ListApi.as_view(),name='list')
+    path('', ListApi.as_view(),name='list'),
+    path('createcart/', CreateCart.as_view(),name='createcart'),
+    path('cartdetail/<int:pk>', CartDetail.as_view(), name='cartdetail'),
+    path('cartdetail/', CartDetail.as_view(), name='cartdetail'),
+    path('likeproduct/', LikeProduct.as_view(), name='likeproduct'),
+    path('voteproduct/<int:pk>', VoteProduct.as_view(), name='voteproduct'),
+    path('comment/<int:pk>', CommentList.as_view(), name='comment')
 ]
